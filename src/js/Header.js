cb9174dc2656d7ada52cd907cf559125f9de711b
JonPlayChannel <165782158+JonPlayChannel@users.noreply.github.com>
2025-10-02 15:28:24 +0300

    Added smooth animation of the appearance and disappearance to the header

--- src/js/Header.js
+++ src/js/Header.js
@@ -5,19 +5,39 @@ const selectors = {
 
 const stateClasses = {
     isActive: 'is-active',
+    isHiding: 'is-hiding'
 }
 
 const rootElement = document.querySelector(selectors.root)
 const logoSvgElement = rootElement.querySelector(selectors.logoSvg)
 
+let lastScrollY = window.scrollY
+
 const handleScroll = () => {
-    if (window.scrollY > 450) {
+    const currentScrollY = window.scrollY
+
+    if (currentScrollY > 450) {
         logoSvgElement.classList.add(stateClasses.isActive)
         rootElement.classList.add(stateClasses.isActive)
+        rootElement.classList.remove(stateClasses.isHiding)
     } else {
+        if (currentScrollY < lastScrollY) {
+            if (rootElement.classList.contains(stateClasses.isActive)) {
+                rootElement.classList.add(stateClasses.isHiding)
+
+                setTimeout(() => {
+                    rootElement.classList.remove(stateClasses.isHiding)
+                }, 1000)
+            }
+        } else {
+            rootElement.classList.remove(stateClasses.isHiding)
+        }
+
         logoSvgElement.classList.remove(stateClasses.isActive)
         rootElement.classList.remove(stateClasses.isActive)
     }
+
+    lastScrollY = currentScrollY
 }
 
 function setHeaderScroll() {
